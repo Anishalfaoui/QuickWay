b51f89ff15a66e524d51bfdc8e8bb445c2e360aa
halfaoui <halfaouianis532@gmail.com>
2024-02-02 22:38:32 +0100

    last

--- client/src/pages/PlacesFormPage.jsx
+++ client/src/pages/PlacesFormPage.jsx
@@ -369,55 +369,9 @@ export default function RidesFormPage() {
           value={price}
           onChange={ev => setPrice(ev.target.value)}
         />
-        <div>photos</div>
-        <div>des photos</div>
-        {addedPhotos.length > 0 &&
-          addedPhotos.map(link => (
-            <div key={link}>
-              <img src={link} alt="" />
-              <button onClick={() => removePhoto(link)}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="w-6 h-6"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                  />
-                </svg>
-              </button>
-            </div>
-          ))}
-        <button>
-          <input
-            type="file"
-            multiple
-            onChange={uploadPhoto}
-            disabled={photoInputDisabled}
-            aria-label="Upload Photos"
-            accept="image/*"
-          />
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-            />
-          </svg>
-          <span> </span> télécharger
-        </button>
+       
+      
+       
         <button className="primary my-4">Save</button>
       </form>
     </div>
